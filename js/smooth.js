4a785af2ff4dd42413365acdfa1691be028c4069
Thiago <thiagotmb6@gmail.com>
2024-05-06 17:43:21 -0300

    Initial Commit

---
+++ js/smooth.js
@@ -0,0 +1,15 @@
+// Smooth Scrolling
+$('#navbar a, .btn').on('click', function (e) {
+  if (this.hash !== '') {
+    e.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100,
+      },
+      800
+    );
+  }
+});
